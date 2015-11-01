81f7ba3c8b41a396f6865da14226b0405ba7db5a
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2015-11-01 19:04:12 +0100

    Issue #1 - Make branch 'wicket-6' buildable/runnable with Java 6

--- wiquery-demo/src/test/java/org/odlabs/wiquery/Start.java
+++ wiquery-demo/src/test/java/org/odlabs/wiquery/Start.java
@@ -1,44 +1,47 @@
 package org.odlabs.wiquery;
 
-import org.eclipse.jetty.server.Connector;
+import org.apache.wicket.util.time.Duration;
 
-import org.eclipse.jetty.server.HttpConnectionFactory;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.bio.SocketConnector;
 import org.eclipse.jetty.webapp.WebAppContext;
 
-public class Start
-{
-	public static void main(String[] args)
-	{
+public class Start {
+	public static void main(String[] args) throws Exception {
+		int timeout = (int) Duration.ONE_HOUR.getMilliseconds();
+
 		Server server = new Server();
-		ServerConnector connector = new ServerConnector(server, new HttpConnectionFactory());
+		SocketConnector connector = new SocketConnector();
+
 		// Set some timeout options to make debugging easier.
-		connector.setIdleTimeout(1000 * 60 * 60);
+		connector.setMaxIdleTime(timeout);
 		connector.setSoLingerTime(-1);
 		connector.setPort(8080);
-		server.setConnectors(new Connector[] {connector});
+		server.addConnector(connector);
 
 		WebAppContext bb = new WebAppContext();
 		bb.setServer(server);
-		bb.setContextPath("/");
+		bb.setContextPath("/wicket");
 		bb.setWar("src/main/webapp");
+
+		// START JMX SERVER
+		// MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+		// MBeanContainer mBeanContainer = new MBeanContainer(mBeanServer);
+		// server.getContainer().addEventListener(mBeanContainer);
+		// mBeanContainer.start();
 
 		server.setHandler(bb);
 
-		try
-		{
+		try {
 			System.out.println(">>> STARTING EMBEDDED JETTY SERVER, PRESS ANY KEY TO STOP");
 			server.start();
 			System.in.read();
 			System.out.println(">>> STOPPING EMBEDDED JETTY SERVER");
 			server.stop();
 			server.join();
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
-			System.exit(100);
+			System.exit(1);
 		}
 	}
 }
